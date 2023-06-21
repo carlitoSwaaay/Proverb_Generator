366377169c739c69de56e06b476a43e55f099287
carlitoSwaaay <cacollier02@yahoo.com>
2023-06-21 10:54:48 -0700

    Update ProverbScreen.js

--- screens/ProverbScreen.js
+++ screens/ProverbScreen.js
@@ -1,7 +1,15 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-const ProverbScreen = () => {
+import { Proverbs } from '../components/Proverbs'
+
+const generateRandomId = (min, max) => {
+  const randomId = Math.floor(Math.random(Proverbs) * (max - min)) + min;
+
+  return randomId;
+
+
+
   return (
     <View>
       <Text>ProverbScreen</Text>
