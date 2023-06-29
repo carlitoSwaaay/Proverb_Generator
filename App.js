50a2fb23297a41ae980725207314409bd5593649
carlitoSwaaay <cacollier02@yahoo.com>
2023-06-29 11:37:58 -0700

    Update App.js

--- App.js
+++ App.js
@@ -50,15 +50,15 @@ export default function App(props) {
 
   return (
     <NavigationContainer>
-      <Stack.Navigator></Stack.Navigator>
       <StatusBar style="light" />
-      <ImageBackground source={require('./assets/2bc93f0f-7867-44d4-bafa-d59f5c51d075.jpeg')}
-        resizeMode="stretch"
-        style={styles.rootScreen}>
-        <SafeAreaView style={styles.rootScreen}>
-          {screen}
-        </SafeAreaView>
-      </ImageBackground>
+      <Stack.Navigator>
+        <ImageBackground source=
+          {require('./assets/2bc93f0f-7867-44d4-bafa-d59f5c51d075.jpeg')}
+          resizeMode="stretch"
+          style={styles.rootScreen}
+        >
+        </ImageBackground>
+      </Stack.Navigator>
     </NavigationContainer>
   );
 };
