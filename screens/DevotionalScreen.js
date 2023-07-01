2f39a695ee09c0c2a0e62d27942d1fd1bd024b30
carlitoSwaaay <cacollier02@yahoo.com>
2023-07-01 13:38:34 -0700

    Update DevotionalScreen.js

---
+++ screens/DevotionalScreen.js
@@ -0,0 +1,18 @@
+import { View, Text, TextBox, } from 'react-native';
+
+const DevotionalScreen = () => {
+
+
+  return (
+    <View>
+      <Text>DevotionalScreen</Text>
+    </View>
+  ),
+
+},
+
+export default DevotionalScreen;
+
+const styles = StyleSheet.create({
+
+});
