f88967772f24bba5f9aeef5fe1f7b79d1ad11cf5
carlitoSwaaay <cacollier02@yahoo.com>
2023-07-01 14:21:51 -0700

    Update DevotionalScreen.js

--- screens/DevotionalScreen.js
+++ screens/DevotionalScreen.js
@@ -4,7 +4,9 @@ import React from 'react'
 const DevotionalScreen = ({ }) => {
   return (
     <View style={styles.container}>
+      <View style={styles.textContainer}>
       <Text>DevotionalScreen</Text>
+      </View>
     </View>
   )
 };
@@ -14,6 +16,11 @@ export default DevotionalScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  textContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 
 });
