24b5e7aeb32eb0e131230b2a53c8ea2d931414e1
carlitoSwaaay <cacollier02@yahoo.com>
2023-07-02 19:11:08 -0700

    Update DevotionalContainer.js

--- components/DevotionalContainer.js
+++ components/DevotionalContainer.js
@@ -1,14 +1,48 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-const DevotionalContainer = () => {
+const Shuffle = require('./ProverbList');
+
+const ProverbContainer = () => {
+
+  const shuffledArray = Shuffle(PROVERBS);
+  console.log(shuffledArray);
+
+
   return (
-    <View>
-      <Text>DevotionalContainer</Text>
-    </View>
+    <SafeAreaView>
+      <View style={styles.container}>
+        <Text style={styles.textContainer}>
+          {shuffledArray[0].proverb}
+        </Text>
+      </View>
+    </SafeAreaView>
   )
-}
 
-export default DevotionalContainer
+};
 
-const styles = StyleSheet.create({})
+export default ProverbContainer;
+
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 20,
+    marginRight: 20,
+    opacity: .9,
+  },
+  textContainer: {
+    backgroundColor: '#d6f2be',
+    borderWidth: 3,
+    borderColor: 'black',
+    borderRadius: 15,
+    padding: 16,
+    fontSize: 18,
+    overflow: 'hidden',
+    fontFamily: 'open-sans-bold',
+
+
+  },
+
+});
