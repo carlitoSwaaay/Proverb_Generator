bdf945b369af04843df0dce6b0cf0d17934c2ba6
carlitoSwaaay <cacollier02@yahoo.com>
2023-07-02 19:06:11 -0700

    added Devotional Container

---
+++ components/DevotionalContainer.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const DevotionalContainer = () => {
+  return (
+    <View>
+      <Text>DevotionalContainer</Text>
+    </View>
+  )
+}
+
+export default DevotionalContainer
+
+const styles = StyleSheet.create({})
--- screens/DevotionalScreen.js
+++ screens/DevotionalScreen.js
@@ -1,16 +1,26 @@
-import { Image, StyleSheet, Text, View, } from 'react-native'
+import { Image, StyleSheet, TextInput, View, } from 'react-native'
 import React from 'react'
+import ProverbContainer from '../components/ProverbContainer';
 
 const DevotionalScreen = () => {
   return (
-
-      <View style={styles.textContainer}>
+    <View style={styles.rootContainer}>
       <View style={styles.backgroundContainer}>
         <Image source=
           {require('../assets/2bc93f0f-7867-44d4-bafa-d59f5c51d075.jpeg')}
           resizeMode='stretch'
           style={styles.backgroundImage}
         />
+      </View>
+
+      <ProverbContainer>
+        <TextInput>Devotional Screen</TextInput>
+      </ProverbContainer>
+
+
+
+      <View style={styles.button}>
+        {/* <BackButton onPress={() => { navigation.navigate('Devotional', { name: Devotional }) }}>Devotional</BackButton> */}
       </View>
     </View>
 
@@ -20,13 +30,34 @@ const DevotionalScreen = () => {
 export default DevotionalScreen;
 
 const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    padding: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    // backgroundColor: '#f8f8f8',
+  },
   container: {
     flex: 1,
   },
   textContainer: {
-    flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    margin: 20,
+    padding: 20,
+
+  },
+  backgroundContainer: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+    // opacity: .5,
+  },
+  backgroundImage: {
+    flex: 1,
+    flexDirection: 'column',
   },
 
 });
