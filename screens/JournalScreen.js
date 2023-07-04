49040f94b603f8aca6510368db832385dd4b545d
carlitoSwaaay <cacollier02@yahoo.com>
2023-07-04 10:23:02 -0700

    Update JournalScreen.js

--- screens/JournalScreen.js
+++ screens/JournalScreen.js
@@ -1,18 +1,45 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { FlatList, Image, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
 const JournalScreen = () => {
   return (
-    <View>
-      <Text>JournalScreen</Text>
+    <View style={styles.rootContainer}>
+      <View style={styles.backgroundContainer}>
+        <Image source=
+          {require('../assets/2bc93f0f-7867-44d4-bafa-d59f5c51d075.jpeg')}
+          resizeMode='stretch'
+          style={styles.backgroundImage}
+        />
+      </View>
+
+      <FlatList>
+        <Text>Journal Entry 1</Text>
+      </FlatList>
+
     </View>
   )
 }
 
-export default JournalScreen
+export default JournalScreen;
 
 const styles = StyleSheet.create({
-  container: {
+  rootContainer: {
     flex: 1,
+    padding: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    // backgroundColor: '#f8f8f8',
+  },
+  backgroundContainer: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+    // opacity: .5,
+  },
+  backgroundImage: {
+    flex: 1,
+    flexDirection: 'column',
   },
 })
