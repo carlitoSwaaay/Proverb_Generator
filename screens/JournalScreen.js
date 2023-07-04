6efc3159bae034676e0b20e70ec129244509adba
carlitoSwaaay <cacollier02@yahoo.com>
2023-07-04 07:42:07 -0700

    Create JournalScreen.js

---
+++ screens/JournalScreen.js
@@ -0,0 +1,18 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const JournalScreen = () => {
+  return (
+    <View>
+      <Text>JournalScreen</Text>
+    </View>
+  )
+}
+
+export default JournalScreen
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+})
